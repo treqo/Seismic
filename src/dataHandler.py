ebb32b2de22a79aa0398792bbceae1c4ca2503f3
LouieTang <louietang2013@gmail.com>
2024-10-05 15:04:56 -0700

    LTA_STA algorithm implemented, fixed double plot

--- src/dataHandler.py
+++ src/dataHandler.py
@@ -6,6 +6,10 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import os
 
+from obspy.signal.invsim import cosine_taper
+from obspy.signal.filter import highpass
+from obspy.signal.trigger import classic_sta_lta, plot_trigger, trigger_onset
+
 class DataHandler:
     def __init__(self, data_directory: str):
         """
@@ -47,7 +51,8 @@ class DataHandler:
         ax.set_ylabel('Velocity (m/s)')
         ax.set_xlabel('Time (s)')
         ax.set_title(f'{self.filename}', fontweight='bold')
-        self.data_stream.plot()
+        
+        plt.show()
 
     def apply_filter(self, filter_type: str, *args):
         """
@@ -71,20 +76,67 @@ class DataHandler:
             tr_copy.filter('highpass', freq=freqmin)
         return tr_copy
     
+
+    def  LTA_STA_detection_algorithm(self, data = None, sta_len = 120, lta_len = 600, thr_on = 4, thr_off = 1.5):
+        if data is not None:
+            tr = data
+        else:
+            tr = self.data_stream.traces[0].copy()
+
+        tr_times = tr.times()
+        tr_data = tr.data
+        # Sampling frequency of our trace
+        df = tr.stats.sampling_rate
+
+        # Run Obspy's STA/LTA to obtain a characteristic function
+        # This function basically calculates the ratio of amplitude between the short-term 
+        # and long-term windows, moving consecutively in time across the data
+        cft = classic_sta_lta(tr_data, int(sta_len * df), int(lta_len * df))
+
+        # Plot characteristic function
+        fig,ax = plt.subplots(1,1,figsize=(12,3))
+        ax.plot(tr_times,cft)
+        ax.set_xlim([min(tr_times),max(tr_times)])
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Characteristic function')
+
+        ax.title.set_text('STA/LTA characteristic function')
+
+        plt.show()
+
+        # Play around with the on and off triggers, based on values in the characteristic function
+        on_off = np.array(trigger_onset(cft, thr_on, thr_off))
+        # The first column contains the indices where the trigger is turned "on". 
+        # The second column contains the indices where the trigger is turned "off".
+
+        # Plot on and off triggers
+        fig,ax = plt.subplots(1,1,figsize=(12,3))
+        for i in np.arange(0,len(on_off)):
+            triggers = on_off[i]
+            ax.axvline(x = tr_times[triggers[0]], color='red', label='Trig. On')
+            ax.axvline(x = tr_times[triggers[1]], color='purple', label='Trig. Off')
+
+        # Plot seismogram
+        ax.plot(tr_times,tr_data)
+        ax.set_xlim([min(tr_times),max(tr_times)])
+        ax.legend()
+
+        plt.show()
+    
 if __name__ == '__main__':
     # Path to the data directory
-    DATA_DIRECTORY = "../data/lunar/training/data/S12_GradeA/xa.s12.00.mhz.1970-06-26HR00_evid00009.mseed"
+    DATA_DIRECTORY = "./data/lunar/training/data/S12_GradeA/xa.s12.00.mhz.1970-06-26HR00_evid00009.mseed"
     # Initialize the DataHandler class
     data_handler = DataHandler(DATA_DIRECTORY)
 
     # Print the metadata of the data stream
     print(data_handler.stats)
 
-    data_handler.get_plot()
-    data_handler.get_plot(data_handler.apply_filter('bandpass', 0.99999, 1))
-    data_handler.get_plot(data_handler.apply_filter('lowpass', 0.1))
-    data_handler.get_plot(data_handler.apply_filter('highpass', 0.9))
-
+    # data_handler.get_plot()
+    # data_handler.get_plot(data_handler.apply_filter('bandpass', 0.99999, 1))
+    # data_handler.get_plot(data_handler.apply_filter('lowpass', 0.1))
+    # data_handler.get_plot(data_handler.apply_filter('highpass', 0.9))
+    data_handler.LTA_STA_detection_algorithm()
 
 
 
